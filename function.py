80c8a58b0da3f865ec59772cb01443c8622e4246
PlugNCS <michael.nass@free.fr>
2020-03-05 16:43:22 +0100

    FORCE UPDATE

--- function.py
+++ function.py
@@ -1,1 +1,49 @@
-#coucou
+
+def choice_perso():
+    choice = ["", ""]
+    choice_flip = ["", ""]
+    print("Joueur 1, choisissez votre personnage,\nsi vous ne connaissez pas la liste des personnages, mettez \"liste\"\n")
+    
+    verif = False
+    while verif == False:
+        j1 = input("...")
+        if j1.lower() == "liste":
+            print("- Sakuya\n- Papacito")
+
+        elif j1.lower() == "sakuya":
+            choice[0] = "/images/sakuya.png"
+            choice_flip[0] = "/images/sakuya_flip.png"
+            verif = True
+
+        elif j1.lower() == "papacito":
+            choice[0] = "/images/papacito.png"
+            choice_flip[0] = "/images/papacito_flip.png"
+            verif = True
+
+        else:
+            print("Je n'ai pas bien compris, veuillez remettre le nom de votre personnage.")
+    
+
+    print("Joueur 2, choisissez votre personnage,\nsi vous ne connaissez pas la liste des personnages, mettez \"liste\"\n")
+    
+    verif = False
+    while verif == False:
+        j2 = input("...")
+        if j2.lower() == "liste":
+            print("- Sakuya\n- Papacito")
+
+        if j2.lower() == "sakuya":
+            choice[1] = "/images/sakuya.png"
+            choice_flip[1] = "/images/sakuya_flip.png"
+            verif = True
+
+        elif j2.lower() == "papacito":
+            choice[1] = "/images/papacito.png"
+            choice_flip[1] = "/images/papacito_flip.png"
+            verif = True
+
+        else:
+            print("Je n'ai pas bien compris, veuillez remettre le nom de votre personnage.")
+
+    full_choice = [choice, choice_flip]
+    return full_choice
--- main.py
+++ main.py
@@ -3,40 +3,50 @@ from pygame.locals import *
 from function import *
 import os
 
- #toutes les coordonnées:
+#toutes les coordonnées:
 
-WINDOW_X = 1000
-WINDOW_Y = 600
+WINDOW_X = 1920
+WINDOW_Y = 1080
 
 BACK_X = 0
-BACK_Y = -400
+BACK_Y = 0
 
-GROUND_Y = 360
+GROUND_Y = 770
 
-
+MOVE = 10 #vitesse de déplacement
+choice = choice_perso()
 
 pygame.init()
 
 
 #création d'une fenetre
-screen = pygame.display.set_mode((WINDOW_X, WINDOW_Y))
+screen = pygame.display.set_mode((WINDOW_X, WINDOW_Y), FULLSCREEN)
 
 curdir = os.path.dirname(os.path.realpath(__file__))
 #chargement des images
 image_wall = pygame.image.load(curdir + "/images/wallpaper.png").convert()
-perso = pygame.image.load(curdir + "/images/sakuya.png").convert_alpha()
-perso_flip = pygame.image.load(curdir + "/images/sakuya_flip.png").convert_alpha()
 
-pos_perso = perso.get_rect()
-heading = 1 #1 = droite; 0 = gauche
+#joueur 1
+j1 = pygame.image.load(curdir + choice[0][0]).convert_alpha()
+j1_flip = pygame.image.load(curdir + choice[1][0]).convert_alpha()
+
+#joueur 2
+j2 = pygame.image.load(curdir + choice[0][1]).convert_alpha()
+j2_flip = pygame.image.load(curdir + choice[1][1]).convert_alpha()
+
+pos_j1 = j1.get_rect()
+pos_j2 = j2.get_rect()
+heading_j1 = 1 #1 = droite; 0 = gauche
+heading_j2 = 0
 
 
-pos_perso = pos_perso.move(0, GROUND_Y-1)
-
+pos_j1 = pos_j1.move(0, GROUND_Y-1)
+pos_j2 = pos_j2.move(1500, GROUND_Y-30)
 
 #placement des images
 screen.blit(image_wall, (BACK_X, BACK_Y))
-screen.blit(perso, pos_perso)
+screen.blit(j1, pos_j1)
+screen.blit(j2, pos_j2)
 
 #raffraichissement de l'écran
 pygame.display.flip()
@@ -52,42 +62,96 @@ while game:
         if event.type == QUIT:
             game = False
 
+    if keys[K_LALT] and keys[K_F4]:
+        game = False
+
+    if keys[K_s]:
+        pos_j1 = pos_j1.move(0, MOVE)
+
+    if keys[K_w]:
+        pos_j1 = pos_j1.move(0, -MOVE)
+    else:
+        pos_j1 = pos_j1.move(0, MOVE)
+
+    if keys[K_d]:
+        pos_j1 = pos_j1.move(MOVE, 0)
+        heading_j1 = 0
+
+    if keys[K_a]:
+        pos_j1 = pos_j1.move(-MOVE, 0)
+        heading_j1 = 1
+    
+
+
+
     if keys[K_DOWN]:
-        pos_perso = pos_perso.move(0, 5)
+        pos_j2 = pos_j2.move(0, MOVE)
 
     if keys[K_UP]:
-        pos_perso = pos_perso.move(0, -5)
+        pos_j2 = pos_j2.move(0, -MOVE)
     else:
-        pos_perso = pos_perso.move(0, 5)
+        pos_j2 = pos_j2.move(0, MOVE)
 
     if keys[K_RIGHT]:
-        pos_perso = pos_perso.move(5, 0)
-        heading = 0
+        pos_j2 = pos_j2.move(MOVE, 0)
+        heading_j2 = 0
 
     if keys[K_LEFT]:
-        pos_perso = pos_perso.move(-5, 0)
-        heading = 1
+        pos_j2 = pos_j2.move(-MOVE, 0)
+        heading_j2 = 1
 
-    if pos_perso.y >= GROUND_Y:
-        pos_perso.y = GROUND_Y-1
 
-    if pos_perso.y <= 0:
-        pos_perso = pos_perso.move(0, 5)
 
-    if pos_perso.x <= 0:
-        pos_perso = pos_perso.move(5, 0)
 
-    if pos_perso.x >= WINDOW_X-110:
-        pos_perso = pos_perso.move(-5, 0)
+    if pos_j1.y >= GROUND_Y:
+        pos_j1.y = GROUND_Y-1
+
+    if pos_j1.y <= 0:
+        pos_j1 = pos_j1.move(0, MOVE)
+
+    if pos_j1.x <= 0:
+        pos_j1 = pos_j1.move(MOVE, 0)
+
+    if pos_j1.x >= WINDOW_X-110:
+        pos_j1 = pos_j1.move(-MOVE, 0)
+    
+
+
+    if pos_j2.y >= GROUND_Y-29:
+        pos_j2.y = GROUND_Y-30
+
+    if pos_j2.y <= 0:
+        pos_j2 = pos_j2.move(0, MOVE)
+
+    if pos_j2.x <= 0:
+        pos_j2 = pos_j2.move(MOVE, 0)
+
+    if pos_j2.x >= WINDOW_X-110:
+        pos_j2 = pos_j2.move(-MOVE, 0)
+
+
+
 
     #recollage des éléments
     screen.blit(image_wall, (BACK_X, BACK_Y))
-    if heading == 1:
-        screen.blit(perso, pos_perso)
+    if heading_j1 == 1:
+        screen.blit(j1, pos_j1)
     else:
-        screen.blit(perso_flip, pos_perso)
+        screen.blit(j1_flip, pos_j1)
+    
+
+
+
+    if heading_j2 == 1:
+        screen.blit(j2, pos_j2)
+    else:
+        screen.blit(j2_flip, pos_j2)
+
+
+
 
     #raffraichissement
+    
     pygame.display.flip()
 
     pygame.time.delay(16)
