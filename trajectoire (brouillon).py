8b62a3ce089471726c2cd7b11c5bf14333e3e127
LaingMASTER7 <nassim.bellik32@gmail.com>
2020-04-24 15:04:02 +0200

    no waaayyyyy

---
+++ trajectoire (brouillon).py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.locals import *
+from function import *
+import os
+import platform
+from math import *
+
+def f(x):
+    return a*X**2+b*X+c
+
+# la vitesse de la balle doit etre réaliste : _ralentie au début si lancer de manière parabolique
+#                                             _accéléré de la pointe vers le sol#
