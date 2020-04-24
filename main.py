5dd20b3a8f71ab33f2f4a219a4462d557ac61bcf
PlugNCS <michael.nass@free.fr>
2020-04-24 13:48:09 +0200

    J'ai déplacé la fonction plus haut et ajouté des données utiles

--- main.py
+++ main.py
@@ -11,6 +11,41 @@ width, height = int(pygame.display.Info().current_w), int(pygame.display.Info().
 WINDOW_X = width
 WINDOW_Y = height
 
+#création d'une fenetre
+if platform.system() == "Linux":
+    screen = pygame.display.set_mode((WINDOW_X, WINDOW_Y))
+else:
+    screen = pygame.display.set_mode((WINDOW_X, WINDOW_Y), FULLSCREEN)
+
+
+"""
+menu = True
+while menu is True:
+    # rectangles choix joueurs
+    size = (width, height)
+    white = pygame.Color(255, 255, 255)
+    red = pygame.Color(255, 0, 0)
+    blue = pygame.Color(0, 0, 255)
+    green = pygame.Color(0, 255, 0)
+    null = pygame.Color(0, 0, 0)
+    button_player1 = pygame.Rect(500, 800, 100, 50)
+    pygame.draw.rect(screen, red, button_player1)
+    button_player2 = pygame.Rect(700, 800, 100, 50)
+    pygame.draw.rect(screen, green, button_player2)
+    button_player3 = pygame.Rect(900, 800, 100, 50)
+    pygame.draw.rect(screen, blue, button_player3)
+    button_player4 = pygame.Rect(1100, 800, 100, 50)
+    pygame.draw.rect(screen, null, button_player4)
+    pygame.display.flip()
+    # Rect en bas (largeur)
+    rect_players_choice = pygame.Rect(6, 700, WINDOW_X, WINDOW_Y) #x , y
+    pygame.draw.rect(screen, color_rect_players, rect_players_choice)
+    # rectangle separation
+    rect_separation = pygame.Rect(int(WINDOW_X/2), 0, 2, int(WINDOW_Y*0.6659) )
+    pygame.draw.rect(screen, red, rect_separation)
+"""
+
+
 BACK_X = 0
 BACK_Y = 0
 
@@ -23,12 +58,6 @@ SIDE_MOVE = 10
 choice = choice_perso()
 
 isMac = platform.system() == "Darwin" or platform.system() == "Linux"
-
-#création d'une fenetre
-if platform.system() == "Linux":
-    screen = pygame.display.set_mode((WINDOW_X, WINDOW_Y))
-else:
-    screen = pygame.display.set_mode((WINDOW_X, WINDOW_Y), FULLSCREEN)
 
 curdir = os.path.dirname(os.path.realpath(__file__))
 #chargement des images
@@ -64,27 +93,6 @@ green = (0, 255, 0)
 blue = (0, 0, 255)
 red = (255, 0, 0)
 null = (153, 122, 144)
-"""
-menu = True
-while menu is True:
-    # rectangles choix joueurs
-    button_player1 = pygame.Rect(500, 800, 100, 50)
-    pygame.draw.rect(screen, red, button_player1)
-    button_player2 = pygame.Rect(700, 800, 100, 50)
-    pygame.draw.rect(screen, green, button_player2)
-    button_player3 = pygame.Rect(900, 800, 100, 50)
-    pygame.draw.rect(screen, blue, button_player3)
-    button_player4 = pygame.Rect(1100, 800, 100, 50)
-    pygame.draw.rect(screen, null, button_player4)
-    pygame.display.flip()
-    # Rect en bas (largeur)
-    rect_players_choice = pygame.Rect(6, 700, WINDOW_X, WINDOW_Y) #x , y 
-    pygame.draw.rect(screen, color_rect_players, rect_players_choice)
-    # rectangle separation
-    rect_separation = pygame.Rect(int(WINDOW_X/2), 0, 2, int(WINDOW_Y*0.6659) ) 
-    pygame.draw.rect(screen, red, rect_separation)
-"""
-
 
 #placement des images
 screen.blit(image_wall, (BACK_X, BACK_Y))
