2d6b177d9efc77f111fe86088093cd0913f7697a
Gyoomu <60664665+Gyoomu@users.noreply.github.com>
2020-06-01 23:33:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2577,7 +2577,7 @@ while on:
                     elif widths[i] > pos_j1.x and widths[i] < pos_j1.x + j1.get_width() and heights[i] > pos_j1.y and heights[i] < pos_j1.y + j1.get_height():
                         #hitbox j1
                         
-                        vie_j1 -= 3
+                        vie_j1 -= 4
                         temps.pop(i)
                         bullets.pop(i)
                         widths.pop(i)
@@ -2592,7 +2592,7 @@ while on:
                     elif widths[i] > pos_j2.x and widths[i] < pos_j2.x + j2.get_width() and heights[i] > pos_j2.y and heights[i] < pos_j2.y + j2.get_height():
                         #hitbox j2
                         
-                        vie_j2 -= 3
+                        vie_j2 -= 4
                         temps.pop(i)
                         bullets.pop(i)
                         widths.pop(i)
@@ -2817,7 +2817,7 @@ while on:
                     elif widths[i] > pos_j1.x and widths[i] < pos_j1.x + j1.get_width() and heights[i] > pos_j1.y and heights[i] < pos_j1.y + j1.get_height():
                         #hitbox j1
                         
-                        vie_j1 -= 3
+                        vie_j1 -= 5
                         temps.pop(i)
                         bullets.pop(i)
                         widths.pop(i)
@@ -2832,7 +2832,7 @@ while on:
                     elif widths[i] > pos_j2.x and widths[i] < pos_j2.x + j2.get_width() and heights[i] > pos_j2.y and heights[i] < pos_j2.y + j2.get_height():
                         #hitbox j2
                         
-                        vie_j2 -= 3
+                        vie_j2 -= 5
                         temps.pop(i)
                         bullets.pop(i)
                         widths.pop(i)
